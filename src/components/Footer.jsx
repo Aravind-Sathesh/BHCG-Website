1d2d8d7db3fcff88784484be6340ca0bba091f96
AravindSathesh <aravind.sathesh@gmail.com>
2024-08-19 01:18:10 +0530

    Added footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import facebookIcon from "../assets/footer/facebook.png";
+import instagramIcon from "../assets/footer/instagram.png";
+import twitterIcon from "../assets/footer/twitter.png";
+import linkedinIcon from "../assets/footer/linkedin.png";
+
+import white_logo from "../assets/white_logo.png";
+import divider from "../assets/footer/divider.png";
+import mailIcon from "../assets/footer/mail.png";
+import phoneIcon from "../assets/footer/phone.png";
+import locationIcon from "../assets/footer/location.png";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <footer className="footer">
+            <div className="lastdiv flex">
+                <div className="icons flex">
+                    <Link to="https://www.instagram.com/the_bhcg/">
+                        <img src={instagramIcon} alt="Instagram" />
+                    </Link>
+                    <Link to="https://www.facebook.com/TheBHCG">
+                        <img src={facebookIcon} alt="Facebook" />
+                    </Link>
+                    <Link to="https://x.com/the_bhcg">
+                        <img src={twitterIcon} alt="Twitter" />
+                    </Link>
+                    <Link to="https://www.linkedin.com/company/bits-hyderabad-consulting-group/">
+                        <img src={linkedinIcon} alt="LinkedIn" />
+                    </Link>
+                </div>
 
-export default Footer
+                <div className="flex navigations">
+                    <img
+                        src={white_logo}
+                        alt="BHCG Logo"
+                        className="footer-logo"
+                    />
+                    <img src={divider} alt="Divider" />
+
+                    <div className="flex-col contacts">
+                        <p className="flex">
+                            <img src={mailIcon} alt="Mail" />
+                            bhcg@hyderabad.bits-pilani.ac.in
+                        </p>
+                        <p className="flex">
+                            <img src={phoneIcon} alt="Phone" />
+                            +91 99999 99999
+                        </p>
+                        <p className="flex">
+                            <img src={locationIcon} alt="Location" />
+                            Hyderabad, IN
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="end flex">© BHCG 2024</div>
+        </footer>
+    );
+};
+
+export default Footer;
