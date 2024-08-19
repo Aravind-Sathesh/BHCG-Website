12adf4286a777bd4068cb78db00adebe3b7a5d8f
AravindSathesh <aravind.sathesh@gmail.com>
2024-08-19 15:20:54 +0530

    Made links open in new tab

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,16 +17,32 @@ const Footer = () => {
         <footer className="FOOTER">
             <div className="lastdiv flex">
                 <div className="icons flex">
-                    <Link to="https://www.instagram.com/the_bhcg/">
+                    <Link
+                        to="https://www.instagram.com/the_bhcg/"
+                        rel="noopener"
+                        target="_blank"
+                    >
                         <img src={instagramIcon} alt="Instagram" />
                     </Link>
-                    <Link to="https://www.facebook.com/TheBHCG">
+                    <Link
+                        to="https://www.facebook.com/TheBHCG"
+                        rel="noopener"
+                        target="_blank"
+                    >
                         <img src={facebookIcon} alt="Facebook" />
                     </Link>
-                    <Link to="https://x.com/the_bhcg">
+                    <Link
+                        to="https://x.com/the_bhcg"
+                        rel="noopener"
+                        target="_blank"
+                    >
                         <img src={twitterIcon} alt="Twitter" />
                     </Link>
-                    <Link to="https://www.linkedin.com/company/bits-hyderabad-consulting-group/">
+                    <Link
+                        to="https://www.linkedin.com/company/bits-hyderabad-consulting-group/"
+                        rel="noopener"
+                        target="_blank"
+                    >
                         <img src={linkedinIcon} alt="LinkedIn" />
                     </Link>
                 </div>
