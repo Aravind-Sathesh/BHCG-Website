a574a0c5235f900e1bb96e037b3e8571ee834668
AravindSathesh <aravind.sathesh@gmail.com>
2024-08-27 14:49:24 +0530

    Updated linkedin logo

--- src/components/Leader.jsx
+++ src/components/Leader.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import linkedinLogo from "../assets/team/linkedinLogo.webp";
+import linkedinLogo from "../assets/team/linkedinLogo.svg";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
