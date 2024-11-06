dc6ac7e0430b69a78f0148448dd61492389b25f6
Aravind Sathesh <aravind.sathesh@gmail.com>
2024-11-06 18:15:29 +0530

    Added events calendar page

---
+++ src/components/EventCard.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Typography, Card, CardContent, Grid } from '@mui/material';
+
+const EventCard = ({ title, date, description, location, time }) => {
+	const [day, month, year] = date.split('/');
+	const monthName = new Date(`${year}-${month}-${day}`).toLocaleString(
+		'default',
+		{ month: 'long' }
+	);
+
+	return (
+		<Card
+			sx={{
+				width: '100%',
+				maxWidth: '90%',
+				margin: '1rem auto',
+				display: 'flex',
+				borderRadius: '10px',
+				boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+				backgroundColor: '#f0fafa',
+				overflow: 'hidden',
+				':hover': {
+					transform: 'scale(1.02)',
+					transition: '0.3s',
+					boxShadow: '0 8px 16px rgba(0, 0, 0, 0.3)',
+				},
+			}}
+		>
+			<Grid container>
+				<Grid
+					item
+					xs={12}
+					sm={3}
+					md={2.5}
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'center',
+						alignItems: 'center',
+						backgroundColor: '#027f7b',
+						color: '#fff',
+						padding: '1rem 0',
+						textAlign: 'center',
+					}}
+				>
+					<Typography variant='h1' sx={{ fontWeight: 'bold' }}>
+						{day}
+					</Typography>
+					<Typography
+						variant='subtitle1'
+						sx={{ fontSize: '1rem', textTransform: 'uppercase' }}
+					>
+						{monthName}
+					</Typography>
+				</Grid>
+
+				<Grid item xs={12} sm={9} md={9.5}>
+					<CardContent>
+						<Typography
+							variant='h5'
+							sx={{
+								color: '#027f7b',
+								fontWeight: 'bold',
+							}}
+						>
+							{title}
+						</Typography>
+						<Typography
+							variant='subtitle1'
+							sx={{
+								color: '#027f7b',
+								fontStyle: 'italic',
+							}}
+						>
+							{time}
+						</Typography>
+						<Typography
+							variant='body2'
+							sx={{
+								marginBottom: '0.5rem',
+								color: '#006d67',
+								fontWeight: 'medium',
+								display: 'inline-block',
+							}}
+						>
+							<strong>Location:</strong> {location}
+						</Typography>
+						<Typography
+							variant='body1'
+							sx={{ color: '#004d4d', lineHeight: 1.6 }}
+						>
+							{description}
+						</Typography>
+					</CardContent>
+				</Grid>
+			</Grid>
+		</Card>
+	);
+};
+
+export default EventCard;
---
+++ src/pages/Events.jsx
@@ -0,0 +1,149 @@
+import React, { useRef } from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import EventCard from '../components/EventCard';
+import { Container, Typography, Box } from '@mui/material';
+
+import events from '../events.json';
+
+import baseline from '../assets/teal_baseline.webp';
+import heading from '../assets/events-heading.webp';
+
+const Events = () => {
+	const footerRef = useRef(null);
+
+	const splitEventsByDate = (events) => {
+		const today = new Date();
+		today.setHours(0, 0, 0, 0);
+
+		const upcomingEvents = [];
+		const pastEvents = [];
+
+		events.forEach((event) => {
+			const [day, month, year] = event.date.split('/');
+			const eventDate = new Date(`${year}-${month}-${day}`);
+
+			if (eventDate >= today) {
+				upcomingEvents.push(event);
+			} else {
+				pastEvents.push(event);
+			}
+		});
+
+		upcomingEvents.sort((a, b) => {
+			const [dayA, monthA, yearA] = a.date.split('/');
+			const [dayB, monthB, yearB] = b.date.split('/');
+			const dateA = new Date(`${yearA}-${monthA}-${dayA}`);
+			const dateB = new Date(`${yearB}-${monthB}-${dayB}`);
+			return dateA - dateB;
+		});
+
+		pastEvents.sort((a, b) => {
+			const [dayA, monthA, yearA] = a.date.split('/');
+			const [dayB, monthB, yearB] = b.date.split('/');
+			const dateA = new Date(`${yearA}-${monthA}-${dayA}`);
+			const dateB = new Date(`${yearB}-${monthB}-${dayB}`);
+			return dateB - dateA;
+		});
+
+		return { upcomingEvents, pastEvents };
+	};
+
+	const { upcomingEvents, pastEvents } = splitEventsByDate(events);
+
+	return (
+		<>
+			<Header type={'light'} footerRef={footerRef} />
+			<div
+				className='heading'
+				style={{ width: '100%', textAlign: 'center' }}
+			>
+				<img
+					src={heading}
+					alt='Heading'
+					style={{ width: '90%', maxWidth: '1200px' }}
+				/>
+			</div>
+			<Container
+				sx={{ width: '100%', marginBottom: { xs: '2rem', md: '0' } }}
+			>
+				<Typography
+					variant='h4'
+					sx={{
+						fontFamily: 'Roboto Serif',
+						textAlign: 'center',
+						color: '#027f7b',
+						fontWeight: 600,
+						fontSize: '2.5rem',
+						padding: '2.5rem 0',
+					}}
+				>
+					Upcoming Events
+				</Typography>
+				<Box
+					sx={{
+						display: 'flex',
+						flexWrap: 'wrap',
+						justifyContent: 'space-around',
+						width: '100%',
+					}}
+				>
+					{upcomingEvents.map((event) => (
+						<EventCard
+							key={event.id}
+							title={event.title}
+							date={event.date}
+							description={event.description}
+							location={event.location}
+							time={event.time}
+						/>
+					))}
+				</Box>
+				<Typography
+					variant='h4'
+					sx={{
+						fontFamily: 'Roboto Serif',
+						textAlign: 'center',
+						color: '#027f7b',
+						fontWeight: 600,
+						fontSize: '2.5rem',
+						padding: '2.5rem 0',
+					}}
+				>
+					Past Events
+				</Typography>
+				<Box
+					sx={{
+						display: 'flex',
+						flexWrap: 'wrap',
+						justifyContent: 'space-around',
+						width: '100%',
+					}}
+				>
+					{pastEvents.map((event) => (
+						<EventCard
+							key={event.id}
+							title={event.title}
+							date={event.date}
+							description={event.description}
+							location={event.location}
+							time={event.time}
+						/>
+					))}
+				</Box>
+				<img
+					className='baseline'
+					src={baseline}
+					alt='Divider'
+					style={{ padding: '3rem 0 4rem 0' }}
+					sx={{
+						display: { xs: 'none', md: 'block' },
+					}}
+				/>
+			</Container>
+			<Footer footerRef={footerRef} />
+		</>
+	);
+};
+
+export default Events;
